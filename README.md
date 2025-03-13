4ad02d659722ffb07a7332d8a1858bc515afa2b9
박준서 <junseopark@bagjunseoui-MacBookAir.local>
2025-03-13 20:13:05 +0900

    Set up routing and update README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# twitter-clone-project
+# 🐦 트위터 클론 코딩 학습 기록  
+
+## 📌 개요  
+이 프로젝트는 **트위터 클론 코딩**을 통해 React를 학습하는 과정입니다.  
+매일 배운 내용을 기록하며, 주요 개념과 구현한 기능을 정리합니다. (수정예정) 
+
+## 🛠️ 기술 스택  
+- Frontend: React  
+
+## 📆 학습 기록  
+
+### 📅 2025-03-13  2.1 Routing
+- React Router 설정 (`react-router-dom` 활용)  
+- `createBrowserRouter`를 사용한 라우팅 구조 생성  
+- `Outlet`을 활용한 레이아웃 컴포넌트 적용  
+- 로그인 및 회원가입 페이지는 레이아웃에서 제외  
+- `styled-components`를 사용한 글로벌 스타일 적용 (`createGlobalStyle`)  
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,51 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./components/layout"
+import Home from "./routes/home"
+import Profile from "./routes/profile"
+import Login from "./routes/login"
+import CreateAccount from "./routes/create-account"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "", //경로를 따로 하지 않았을때는 현재 위치 
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ]
+  },
+  //login과 create-account는 layout에 감싸지 않고 따로 빼줌 
+  //layout은 login한 계정만 볼 수 있게 하는 장치
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />
+  }
+])
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+   *{
+  box-sizing: border-box;
+  }
+  body{
+    background-color: black;
+    color: white;
+    font
+
+ 
+`;
 
 
 
@@ -6,7 +54,8 @@ function App() {
 
   return (
     <>
-
+      <GlobalStyles />
+      <RouterProvider router={router} />
     </>
   )
 }
