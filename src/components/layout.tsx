ef01dd229451c15db057c825642507b90f99186e
박준서 <pjseo1313@daum.net>
2025-03-29 20:27:04 +0900

    feat(layout): 사이드 내비게이션 UI 및 로그아웃 기능 추가
    - CSS Grid를 활용한 Layout 구성 (사이드바 + 메인 콘텐츠)
    - Home, Profile, Logout 메뉴 아이콘 적용
    - 로그아웃 버튼 클릭 시 confirm() 팝업 및 Firebase signOut 적용
    - 로그아웃 시 로그인 페이지로 리다이렉트 처리
    - Layout 컴포넌트 내에 Protected Routes 감싸는 구조 완성

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,85 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import { auth } from "../firebase";
 
-export default function Layout(){
-    return(
-        <>
-        <Outlet />
-        </>
+const Wrapper = styled.div`
+    display: grid;
+    gap: 20px;
+    grid-template-columns: 1fr 4fr;
+    height:100%;
+    padding: 50px 0px;
+    width: 100%;
+    max-width: 860px;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`;
+
+const MenuItem = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid white;
+    height: 50px;
+    width: 50px;
+    border-radius: 50%;
+
+    svg {
+        width: 30px;
+        fill: white;
+    }
+
+    &.log-out {
+        border-color: tomato;
+
+        svg {
+            fill: tomato;
+        }
+    }
+`;
+
+
+export default function Layout() {
+    const navigate = useNavigate();
+    const onLogout = async () => {
+        const ok = confirm("Are you sure you want to log out?");
+
+        if(ok){
+            await auth.signOut();
+            navigate("/login");
+        }
+    }
+  
+    return (
+        <Wrapper>
+            <Menu>
+                <Link to="/">
+                    <MenuItem>
+                        <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path clipRule="evenodd" fillRule="evenodd" d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z" />
+                        </svg>
+                    </MenuItem>
+                </Link>
+                <Link to="/profile">
+                    <MenuItem>
+                        <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path d="M7 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM14.5 9a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM1.615 16.428a1.224 1.224 0 0 1-.569-1.175 6.002 6.002 0 0 1 11.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 0 1 7 18a9.953 9.953 0 0 1-5.385-1.572ZM14.5 16h-.106c.07-.297.088-.611.048-.933a7.47 7.47 0 0 0-1.588-3.755 4.502 4.502 0 0 1 5.874 2.636.818.818 0 0 1-.36.98A7.465 7.465 0 0 1 14.5 16Z" />
+                        </svg>
+                    </MenuItem>
+                </Link>
+                <MenuItem onClick={onLogout} className="log-out">
+                    <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path clipRule="evenodd" fillRule="evenodd" d="M3 4.25A2.25 2.25 0 0 1 5.25 2h5.5A2.25 2.25 0 0 1 13 4.25v2a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 0-.75-.75h-5.5a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 0 0 .75-.75v-2a.75.75 0 0 1 1.5 0v2A2.25 2.25 0 0 1 10.75 18h-5.5A2.25 2.25 0 0 1 3 15.75V4.25Z" />
+                        <path clipRule="evenodd" fillRule="evenodd" d="M19 10a.75.75 0 0 0-.75-.75H8.704l1.048-.943a.75.75 0 1 0-1.004-1.114l-2.5 2.25a.75.75 0 0 0 0 1.114l2.5 2.25a.75.75 0 1 0 1.004-1.114l-1.048-.943h9.546A.75.75 0 0 0 19 10Z" />
+                    </svg>
+                </MenuItem>
+            </Menu>
+            <Outlet />
+        </Wrapper>
 
     );
 }
